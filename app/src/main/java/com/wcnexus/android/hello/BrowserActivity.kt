030160a094db85761638ddd40b88e829697f04eb
longyiszh <longyiszh@gmail.com>
2018-11-20 14:35:23 -0500

    browser activity basics

---
+++ app/src/main/java/com/wcnexus/android/hello/BrowserActivity.kt
@@ -0,0 +1,12 @@
+package com.wcnexus.android.hello
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BrowserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browser)
+    }
+}
